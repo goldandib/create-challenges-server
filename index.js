a4a7a4a37c0488b58ff9a41c51d2a296890a2115
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-02-28 09:45:08 +0100

    add contact and privacy

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ require('@entkenntnis/challenges-server')(config => {
   // config.locale = 'de
   // config.brand = 'My-Server'
   // config.slogan = 'Learning by challenges!'
+  /* config.i18nExtend.push({
+    lng: 'de',
+    key: 'contact.content_',
+    value: '<h1>Kontaktinformationen</h1>'
+  }) */
+  /* config.i18nExtend.push({
+    lng: 'de',
+    key: 'privacy.content_',
+    value: '<h1>Datenschutzerklärung</h1>'
+  }) */
   return config
 })
